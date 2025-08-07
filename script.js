9e9966eaa3b49ce1331a5cb43f92e5872a0a307a
kamyla-vandoski <vandoski.kamyla@escola.pr.gov.br>
2025-08-07 08:22:43 -0300

    kk

---
+++ script.js
@@ -0,0 +1,7 @@
+contbotãoBostraPalavras=document.querySelector('#botãopalavrachve');
+
+botãoMostraPalavras.addEventlistener('click, mostraPalavrasChave');
+
+function mostraPalavrasChave(){
+    alert('Fui clicado!');
+}
